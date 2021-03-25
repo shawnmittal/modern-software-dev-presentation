6ad4398aac704303e454137db7a358c9cc0b2c5a
shawn <shawn@pop-os.localdomain>
2021-03-25 08:59:01 -0400

    added sample web app

---
+++ sample_webapp/tests/app_test.py
@@ -0,0 +1,15 @@
+import pytest
+
+from app import main
+
+@pytest.fixture
+def flask_app():
+    yield main
+
+@pytest.fixture
+def client(flask_app):
+    return flask_app.test_client()
+
+def test_index(flask_app, client):
+    res = client.get('/')
+    assert res.status_code == 200
